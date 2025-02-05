d0f25166d8f2a27f7e84c5ebb29e39a6f59f2b25
Muhammad Ilham Ashiddiq Tresnawan <m477ashiddiq@gmail.com>
2025-02-05 15:17:46 +0700

    create facematch

--- src/pages/face_auth.py
+++ src/pages/face_auth.py
@@ -2,67 +2,72 @@ import streamlit as st
 import cv2
 # from PIL import Image
 
-def face_recog():
-    # Streamlit UI
-    st.title("Web Attendance System")
+class FaceMatch:
+    
+    def __init__(self):
+        pass
 
-    # Start the webcam
-    cap = cv2.VideoCapture(0)
+    def face_recog():
+        # Streamlit UI
+        st.title("Web Attendance System")
 
-    faceCascade = cv2.CascadeClassifier("./artifacts/cascade/haarcascade_frontalface_default.xml")
+        # Start the webcam
+        cap = cv2.VideoCapture(0)
 
-    col1, col2, col3 = st.columns([1, 2, 1])
+        faceCascade = cv2.CascadeClassifier("./artifacts/cascade/haarcascade_frontalface_default.xml")
 
-    with col2:
-        frame_placeholder = st.empty()
-        frame_placeholder.image("./artifacts/user.jpg", channels="BGR", use_container_width=True)
-        
-    st.write("")  
-    st.write("")
+        col1, col2, col3 = st.columns([1, 2, 1])
 
-    # Streamlit button to capture an image
-    start = st.button("Start Camera")
-    capture = False
-
-    if start:
-        while cap.isOpened():
-            ret, frame = cap.read()
+        with col2:
+            frame_placeholder = st.empty()
+            frame_placeholder.image("./artifacts/user.jpg", channels="BGR", use_container_width=True)
             
-            if not ret:
-                st.error("Failed to capture image")
-                break
-            
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        st.write("")  
+        st.write("")
 
-            faces = faceCascade.detectMultiScale(
-                gray,
-                scaleFactor=1.1, # change to 1.2 if there any error detecting face due image quality 
-                minNeighbors=5,
-                minSize=(30, 30)
-            )
-            
-            for (x, y, w, h) in faces:
-                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-                print(f'width: {w}, height: {h}')
-                if w >= 300 and h >= 300:
-                    face_roi = frame[y:y+h+50, x:x+w+50]
-                    print(type(face_roi))
-                    # cv2.imwrite("detected_face.jpg", face_roi)
-                    capture = True
-                    
-            
-            # Convert BGR (OpenCV) to RGB (Streamlit)
-            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            
-            # Display the live frame in Streamlit
-            with col2:
-                frame_placeholder.image(frame_rgb, channels="RGB", use_container_width=True)
+        # Streamlit button to capture an image
+        start = st.button("Start Camera")
+        capture = False
+
+        if start:
+            while cap.isOpened():
+                ret, frame = cap.read()
                 
-            if capture:
-                break
+                if not ret:
+                    st.error("Failed to capture image")
+                    break
+                
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Release webcam
-    cap.release()
+                faces = faceCascade.detectMultiScale(
+                    gray,
+                    scaleFactor=1.1, # change to 1.2 if there any error detecting face due image quality 
+                    minNeighbors=5,
+                    minSize=(30, 30)
+                )
+                
+                for (x, y, w, h) in faces:
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                    print(f'width: {w}, height: {h}')
+                    if w >= 300 and h >= 300:
+                        face_roi = frame[y:y+h+50, x:x+w+50]
+                        print(type(face_roi))
+                        # cv2.imwrite("detected_face.jpg", face_roi)
+                        capture = True
+                        
+                
+                # Convert BGR (OpenCV) to RGB (Streamlit)
+                frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                
+                # Display the live frame in Streamlit
+                with col2:
+                    frame_placeholder.image(frame_rgb, channels="RGB", use_container_width=True)
+                    
+                if capture:
+                    break
 
-    with col2:
-        frame_placeholder.image("./artifacts/user.jpg", channels="BGR", use_container_width=True)
+        # Release webcam
+        cap.release()
+
+        with col2:
+            frame_placeholder.image("./artifacts/user.jpg", channels="BGR", use_container_width=True)
