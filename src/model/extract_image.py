7ca88df7eebe1a13f371b8c06a2a98d9091dd9f0
Muhammad Ilham Ashiddiq Tresnawan <m477ashiddiq@gmail.com>
2025-02-12 18:24:40 +0700

    Update extract_image.py
    add model embedder

--- src/model/extract_image.py
+++ src/model/extract_image.py
@@ -1,10 +1,12 @@
 from PIL import Image
-from facenet_pytorch import MTCNN
+import torch.nn.functional as F
+from facenet_pytorch import MTCNN, InceptionResnetV1
 
 class Extract:
     def __init__(self):
         self.mtcnn = MTCNN()
         self.extracted_faces = []
+        self.embed_faces = []
 
     def get_face(self, images, isList=True):
         if isList:
@@ -16,4 +18,17 @@ class Extract:
             self.extracted_faces.append(self.mtcnn(im).unsqueeze(0))
         
         return self.extracted_faces
+    
+    def extract(self, isList=True):
+        resnet = InceptionResnetV1(pretrained='casia-webface').eval()
+        if isList:
+            for face in self.extracted_faces:
+                embeding = resnet(face).detach()
+                embeddings1 = F.normalize(embeding, p=2, dim=1)
+                self.embed_faces.append(embeddings1)
+        else:
+            embeding = resnet(self.extracted_faces[0]).detach()
+            embeddings1 = F.normalize(embeding, p=2, dim=1)
+            self.embed_faces.append(embeddings1) 
+        
     
