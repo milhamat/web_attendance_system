8cec8a702b80448ad443f4f4f5e021ecb378a42f
Muhammad Ilham Ashiddiq Tresnawan <m477ashiddiq@gmail.com>
2025-02-12 18:33:18 +0700

    Update similarity.py
    add cosine similarity

--- src/model/similarity.py
+++ src/model/similarity.py
@@ -1,7 +1,21 @@
-
+import torch
 class SimilarityFace:
     def __init__(self):
-        pass    
+        self.status = ''
+    
+    def count_similarity(self, query_vector, current_vector):
+        
+        similarity = torch.nn.functional.cosine_similarity(query_vector, current_vector).item()
+        # Convert to cosine distance (1 - similarity)
+        cosine_distance = 1 - similarity
+
+        # Set a threshold for verification
+        threshold = 0.4  # Adjust based on your dataset
+        if cosine_distance < threshold:
+            self.status = 'Same person'
+        else:
+            self.status = 'Different persons'
+        return self.status    
     
 # Query vector image 
 
