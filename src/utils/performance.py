8e4efad4a5eef1305760af16fdcc020d4d006e27
Muhammad Ilham Ashiddiq Tresnawan <m477ashiddiq@gmail.com>
2025-02-15 12:34:26 +0700

    succes integrate mlflow

--- src/utils/performance.py
+++ src/utils/performance.py
@@ -1,15 +1,66 @@
 import mlflow
+import random
+import time
+import numpy as np
+from itertools import combinations
 from src.sql.image import ImageEmbedd
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score, recall_score, f1_score
 
 img_embed = ImageEmbedd()
+numbers = range(5)
+pairs = combinations(numbers, 2)
+THRESHOLD = 0.4
+y_label = lst = [1] * 10 + [0] * 10
+pred = []
+start_time = time.time()
 
 face1 = img_embed.image_fetch("1") # each has 5 face
 face2 = img_embed.image_fetch("2")
 
+def embed_convert(vector_embed):
+        conv = list(map(float, vector_embed.strip("[]").split(",")))
+        return np.array(conv, dtype=np.float32) 
+
+def count_similarity(query_vector: np.ndarray, current_vector: np.ndarray, thres:float) -> float:
+        dot_product = np.dot(query_vector, current_vector)
+        norm_product = np.linalg.norm(query_vector) * np.linalg.norm(current_vector)
+        
+        similarity = 1 - (dot_product / norm_product)
+        
+        threshold = thres # 0.4
+        if similarity < threshold:
+            status = 1
+        else:
+            status = 0
+        return status  
+
+# SIMIILAR FACE
+for a, b in pairs:
+    f1 =  embed_convert(face1[a][1])
+    f2 =  embed_convert(face1[b][1])
+    pred.append(count_similarity(f1, f2, THRESHOLD))  
+    
+# DISIMILAR FACE
+for n in range(10):
+    f1 =  embed_convert(face1[random.randint(0, 4)][1])
+    f2 =  embed_convert(face2[random.randint(0, 4)][1])
+    pred.append(count_similarity(f1, f2, THRESHOLD))
+    
+end_time = time.time()
+accuracy = accuracy_score(y_label, pred)
+precision = precision_score(y_label, pred)
+recall = recall_score(y_label, pred)
+f1 = f1_score(y_label, pred)
+response_time = end_time - start_time
+
 mlflow.set_tracking_uri('sqlite:///mlflow.db')
 mlflow.set_experiment("web_attendance_system")
-mlflow.enable_system_metrics_logging()
 
-# mlflow.log_param("Threshold", threshold)
-# mlflow.log_metric("Cosine Similarity", similarity)
-# mlflow.log_param("Match", is_match)
+with mlflow.start_run():
+    mlflow.log_param("Threshold", THRESHOLD)
+    mlflow.log_metric("Accuracy", accuracy)
+    mlflow.log_metric("Precision", precision)
+    mlflow.log_metric("Recall", recall)
+    mlflow.log_metric("F1", f1)
+    mlflow.log_metric("response_time", response_time)
